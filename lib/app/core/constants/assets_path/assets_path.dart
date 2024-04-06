4821773edd31aef0166f26c4a5c73dde9351f5d4
Rahul <rahulsharma3258work@gmail.com>
2024-04-06 15:30:00 +0530

    created Assets path variables to store slidder images, menu images & food offers images Path |

--- lib/app/core/constants/assets_path/assets_path.dart
+++ lib/app/core/constants/assets_path/assets_path.dart
@@ -1,9 +1,40 @@
 class AssetsPaths {
-  // AppBar Assets Path
+  // AppBar
   static const String titleLogo =
       'assets/Images/Logo Images/Title Logo/title_logo.png';
 
-  // Bottom Logo Image Assets Path
+  // Bottom Logo Image
   static const String bottomLogoImage =
       'assets/Images/Logo Images/Bottom Image Logo with Text/bottom_logo_image.png';
+
+  // Slider Images
+  static const String sliderImage_1 = '';
+  static const String sliderImage_2 = '';
+  static const String sliderImage_3 = '';
+
+  // Menu Images
+  static const String burger = '';
+  static const String chinese = '';
+  static const String coolers = '';
+  static const String deserts = '';
+  static const String dips = '';
+  static const String fries = '';
+  static const String hotBaverages = '';
+  static const String jumboSpecial = '';
+  static const String jums = '';
+  static const String maggi = '';
+  static const String meals = '';
+  static const String pastas = '';
+  static const String pizza7inch = '';
+  static const String pizza10inch = '';
+  static const String rice = '';
+  static const String salad = '';
+  static const String shakesAndBaverages = '';
+  static const String toasts = '';
+  static const String wraps = '';
+
+  // Food Offers Images
+  static const String foodOffer_1 = '';
+  static const String foodOffer_2 = '';
+  static const String foodOffer_3 = '';
 }
