79736e3cabb6b796223b8178a5979f89206b0703
Rahul <rahulsharma3258@gmail.com>
2024-04-06 14:20:51 +0530

    imported get package | changed MaterialApp to GetMaterialApp to access GetX State Management full Functionalities  |

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,7 +10,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       title: 'Jumbo Foods',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
