aa5144a554400dda9f8d1f2bfe4af044f92c2728
Rahul <rahulsharma3258work@gmail.com>
2024-04-06 15:13:25 +0530

    Created AssetsPaths class to store path of all local assets of our app | so that we can directly access them through this class |

---
+++ lib/app/core/constants/assets_path/assets_path.dart
@@ -0,0 +1,9 @@
+class AssetsPaths {
+  // AppBar Assets Path
+  static const String titleLogo =
+      'assets/Images/Logo Images/Title Logo/title_logo.png';
+
+  // Bottom Logo Image Assets Path
+  static const String bottomLogoImage =
+      'assets/Images/Logo Images/Bottom Image Logo with Text/bottom_logo_image.png';
+}
