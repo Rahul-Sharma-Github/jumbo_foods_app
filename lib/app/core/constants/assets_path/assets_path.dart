0d932b7b02cb06960402520d8cf41b405d4e31ad
Rahul <rahulsharma3258work@gmail.com>
2024-04-07 09:23:40 +0530

    added all remaining assets path to AssetsPaths class |

--- lib/app/core/constants/assets_path/assets_path.dart
+++ lib/app/core/constants/assets_path/assets_path.dart
@@ -8,33 +8,40 @@ class AssetsPaths {
       'assets/Images/Logo Images/Bottom Image Logo with Text/bottom_logo_image.png';
 
   // Slider Images
-  static const String sliderImage_1 = '';
-  static const String sliderImage_2 = '';
-  static const String sliderImage_3 = '';
+  static const String sliderImage_1 = 'assets/Images/Slider Images/1.jpg';
+  static const String sliderImage_2 = 'assets/Images/Slider Images/2.jpg';
+  static const String sliderImage_3 = 'assets/Images/Slider Images/3.jpg';
 
   // Menu Images
-  static const String burger = '';
-  static const String chinese = '';
-  static const String coolers = '';
-  static const String deserts = '';
-  static const String dips = '';
-  static const String fries = '';
-  static const String hotBaverages = '';
-  static const String jumboSpecial = '';
-  static const String jums = '';
-  static const String maggi = '';
-  static const String meals = '';
-  static const String pastas = '';
-  static const String pizza7inch = '';
-  static const String pizza10inch = '';
-  static const String rice = '';
-  static const String salad = '';
-  static const String shakesAndBaverages = '';
-  static const String toasts = '';
-  static const String wraps = '';
+  static const String burger = 'assets/Images/Menu Images/Burger.jpg';
+  static const String chinese = 'assets/Images/Menu Images/Chinese.jpg';
+  static const String coolers = 'assets/Images/Menu Images/Coolers.jpg';
+  static const String deserts = 'assets/Images/Menu Images/Deserts.jpg';
+  static const String dips = 'assets/Images/Menu Images/Dips.jpg';
+  static const String fries = 'assets/Images/Menu Images/Fries.jpg';
+  static const String hotBaverages =
+      'assets/Images/Menu Images/Hot Baverages.jpg';
+  static const String jumboSpecial =
+      'assets/Images/Menu Images/Jumbo_Special.jpg';
+  static const String jums = 'assets/Images/Menu Images/Jums.jpg';
+  static const String maggi = 'assets/Images/Menu Images/Maggi.jpg';
+  static const String meals = 'assets/Images/Menu Images/Meals.jpg';
+  static const String pastas = 'assets/Images/Menu Images/Pastas.jpg';
+  static const String pizza7inch = 'assets/Images/Menu Images/Pizza 7inch.jpg';
+  static const String pizza10inch =
+      'assets/Images/Menu Images/Pizza 10inch.jpg';
+  static const String rice = 'assets/Images/Menu Images/Rice.jpg';
+  static const String salad = 'assets/Images/Menu Images/Salad.jpg';
+  static const String shakesAndBaverages =
+      'assets/Images/Menu Images/Shakes_&_Baverages.jpg';
+  static const String toasts = 'assets/Images/Menu Images/Toasts.jpg';
+  static const String wraps = 'assets/Images/Menu Images/Wraps.jpg';
 
   // Food Offers Images
-  static const String foodOffer_1 = '';
-  static const String foodOffer_2 = '';
-  static const String foodOffer_3 = '';
+  static const String foodOffer_1 =
+      'assets/Images/Food Offers Images/offer_1.jpg';
+  static const String foodOffer_2 =
+      'assets/Images/Food Offers Images/offer_2.jpg';
+  static const String foodOffer_3 =
+      'assets/Images/Food Offers Images/offer_3.jpg';
 }
