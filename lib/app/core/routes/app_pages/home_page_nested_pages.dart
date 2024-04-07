58d0418640862c377ce27b7bf5a6d35dbe397fdd
Rahul <rahulsharma3258work@gmail.com>
2024-04-07 18:40:44 +0530

    added FoodCategoryPage and SearchPage as a Nested Page for HomePage |

--- lib/app/core/routes/app_pages/home_page_nested_pages.dart
+++ lib/app/core/routes/app_pages/home_page_nested_pages.dart
@@ -1,9 +1,12 @@
 import 'package:get/get.dart';
 
+import '../../../features/users_panel/food_category/presentation/pages/food_category_page.dart';
+import '../../../features/users_panel/search/presentation/pages/search_page.dart';
+
 // Nested pages for HomePage
 final List<GetPage> homePageNestedPages = [
-  // GetPage(name: '/food_category', page: () => FoodCategoryPage()),
-  // GetPage(name: '/search', page: () => SearchPage()),
+  GetPage(name: '/food_category', page: () => const FoodCategoryPage()),
+  GetPage(name: '/search', page: () => const SearchPage()),
 
   // Add more nested pages as needed
 ];
