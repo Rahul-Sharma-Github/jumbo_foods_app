92d931b77c514b296cce207f0a092d56386642bb
Rahul <rahulsharma3258work@gmail.com>
2024-04-07 18:39:22 +0530

    added SearchPage as a nested page for FoodOffersPage |

--- lib/app/core/routes/app_pages/food_offers_nested_pages.dart
+++ lib/app/core/routes/app_pages/food_offers_nested_pages.dart
@@ -1,8 +1,10 @@
 import 'package:get/get.dart';
 
+import '../../../features/users_panel/search/presentation/pages/search_page.dart';
+
 // Nested pages for FoodOffersPage
 final List<GetPage> foodOffersNestedPages = [
-  // GetPage(name: '/search', page: () => SearchPage()),
+  GetPage(name: '/search', page: () => const SearchPage()),
 
   // Add more nested pages as needed
 ];
