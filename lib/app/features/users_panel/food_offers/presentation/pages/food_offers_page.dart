8d781bc8a4f2634457253a9ae08669dc5b48dde7
Rahul <rahulsharma3258work@gmail.com>
2024-04-07 18:29:43 +0530

    created FoodOffersPage |

---
+++ lib/app/features/users_panel/food_offers/presentation/pages/food_offers_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class FoodOffersPage extends StatelessWidget {
+  const FoodOffersPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder(
+      child: Center(
+        child: Text('Food Offers Page'),
+      ),
+    );
+  }
+}
