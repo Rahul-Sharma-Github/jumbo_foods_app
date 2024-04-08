5b4d48567be46a254c6b096cdd80dd0407df0625
Rahul <rahulsharma3258work@gmail.com>
2024-04-08 15:23:24 +0530

    Implemented PopScope() to handle the Navigator Pop Operation Manually |

--- lib/app/features/users_panel/home/presentation/pages/home_page.dart
+++ lib/app/features/users_panel/home/presentation/pages/home_page.dart
@@ -11,8 +11,29 @@ class HomePage extends StatelessWidget {
   final HomePageController homePageController = Get.put(HomePageController());
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: appBar(),
-    );
+    return PopScope(
+        canPop: true,
+        onPopInvoked: (didPop) async {
+          // if true then return nothing
+          if (didPop) {
+            return;
+          }
+          // else
+          // getting delegate
+          final Routing delegate = Get.routing;
+          // checking if any Dialog or BottomSheet is Open the first close them
+          if (delegate.args?.isDialog! || delegate.args?.isBottomSheet!) {
+            Get.back(
+                closeOverlays:
+                    true); // Allow system pop if dialog, snackbar, or bottom sheet is open
+          } else {
+            // Handle back button manually
+            // accessing current Navigation's navigatorKey and Poping a Page from this Navigator
+            !await delegate.args?.navigatorKey.currentState?.maybePop();
+          }
+        },
+        child: Scaffold(
+          appBar: appBar(),
+        ));
   }
 }
