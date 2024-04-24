738163bf704eb4e9ee2f5f6e475a82e33deb2aed
Rahul <rahulsharma3258work@gmail.com>
2024-04-24 10:49:17 +0530

    deleted unused imports from main.dart file |

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:jumbo_foods_app/app/features/users_panel/home/presentation/pages/home_wrapper_.dart';
-// import 'package:jumbo_foods_app/app/features/users_panel/instagram/presentation/pages/instagram_page.dart';
-// import 'package:jumbo_foods_app/app/features/users_panel/profile/presentation/pages/profile_page.dart';
-// import 'package:jumbo_foods_app/app/features/users_panel/whataspp/presentation/pages/whatsapp_page.dart';
-
-// import 'app/core/routes/app_pages/food_offers_nested_pages.dart';
-// import 'app/core/routes/app_pages/home_page_nested_pages.dart';
-// import 'app/features/users_panel/food_offers/presentation/pages/food_offers_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
