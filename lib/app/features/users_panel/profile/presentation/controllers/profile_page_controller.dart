c3cc4e8eb6bb501b965bf6287ab1986a65d88d21
Rahul <rahulsharma3258work@gmail.com>
2024-04-25 20:14:55 +0530

    Added Google Map Launcher Button with Logic to Launch the App with specified location in Profile Page |

--- lib/app/features/users_panel/profile/presentation/controllers/profile_page_controller.dart
+++ lib/app/features/users_panel/profile/presentation/controllers/profile_page_controller.dart
@@ -1,12 +1,5 @@
-import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class ProfilePageController extends GetxController {
-  // Google Map using HTML Embed
-  Widget showMap(String url) {
-    //
-    return HtmlElementView(
-      viewType: url,
-    );
-  }
+  //
 }
--- lib/app/features/users_panel/profile/presentation/pages/profile_page.dart
+++ lib/app/features/users_panel/profile/presentation/pages/profile_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:jumbo_foods_app/app/core/constants/theme/textstyles/textstyle.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 import '../../../../../core/constants/assets_path/assets_path.dart';
 import '../controllers/profile_page_controller.dart';
@@ -39,9 +40,28 @@ class ProfilePage extends StatelessWidget {
               SizedBox(
                 height: 250,
                 width: MediaQuery.of(context).size.width,
-                child: const Card(
+                child: Card(
                   margin: EdgeInsets.zero,
-                  child: Text('Google Map'),
+                  child: Center(
+                    child: OutlinedButton(
+                      style: const ButtonStyle(),
+                      onPressed: () async {
+                        // Launching Google Map App with Specified Location
+                        const String mapUrl =
+                            "https://maps.app.goo.gl/KiQgkUd3nsGJgUoE6";
+                        if (mapUrl.isNotEmpty) {
+                          await launchUrl(Uri.parse(mapUrl));
+                        } else {
+                          Get.snackbar(
+                            'App Not Available',
+                            'Install Google Map First !',
+                            backgroundColor: Colors.orange[300],
+                          );
+                        }
+                      },
+                      child: const Text('See Direction on Google Map'),
+                    ),
+                  ),
                 ),
               ),
               const SizedBox(height: 40),
