296c9093b2f873c9627b8e1b98cc7211e1c656a5
Rahul <rahulsharma3258work@gmail.com>
2024-04-25 14:30:28 +0530

    added flutter_inappwebview package dependency to app |

--- lib/app/features/users_panel/profile/presentation/controllers/profile_page_controller.dart
+++ lib/app/features/users_panel/profile/presentation/controllers/profile_page_controller.dart
@@ -1,5 +1,12 @@
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class ProfilePageController extends GetxController {
-  //
+  // Google Map using HTML Embed
+  Widget showMap(String url) {
+    //
+    return HtmlElementView(
+      viewType: url,
+    );
+  }
 }
