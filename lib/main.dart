2662def04cf5d1699046395a5acaeea3c4a6f840
Rahul <rahulsharma3258work@gmail.com>
2024-05-18 16:07:31 +0530

    set SmartManagement property to it's default value for GetMaterialApp |

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      smartManagement: SmartManagement.full,
+      // smartManagement: SmartManagement.full,
       debugShowCheckedModeBanner: false,
       title: 'Jumbo Foods',
       theme: ThemeData(
