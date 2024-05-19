6fa3dffa6b8c646c79ef24b86f991c13ebb4cf18
Rahul <rahulsharma3258work@gmail.com>
2024-05-19 08:24:41 +0530

    Added comments inside ApiEndpoints Class |

--- lib/app/core/api_source/api_endpoints.dart
+++ lib/app/core/api_source/api_endpoints.dart
@@ -1,11 +1,16 @@
 class ApiEndpoints {
+  // Base URL
   static const String baseUrl =
       'https://nodejs-server-jumbo-foods.onrender.com'; // Render Hosted Link
+
+  // Other API Endpoints
   static const String createMenuItem = '/create-menu-item';
   static const String getAllMenuItems = '/all-menu-items';
   static const String getAllMenuItemsByCategory = '/menu-items';
   static const String updateMenuItem = '/update-menu-item';
   static const String deleteMenuItem = '/delete-menu-item';
+
+  // Connection Timeout & Receive Timeout Durations
   static const Duration connectionTimeout = Duration(milliseconds: 5000);
   static const Duration receiveTimeout = Duration(milliseconds: 5000);
 }
